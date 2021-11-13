3e375cd7f58f72f48cd00340265c444aa045d008
iaprofessional <75013595+iaprofessional@users.noreply.github.com>
2021-11-13 11:53:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,166 @@
-img = ""
-Status = ""
-ringtone = "ringtone.mp3"
-objects = [];
-function setup() {
-    canvas = createCanvas(380, 380);
-    canvas.center();
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status: Baby has not been found!"
-    ringtone.play();
+var paddle2 =10,paddle1=10;
+
+var paddle1X = 10,paddle1Height = 110;
+var paddle2Y = 685,paddle2Height = 70;
+
+var score1 = 0, score2 =0;
+var paddle1Y;
+
+var  playerscore =0;
+var audio1;
+var pcscore =0;
+//ball x and y and speedx speed y and radius
+var ball = {
+    x:350/2,
+    y:480/2,
+    r:20,
+    dx:3,
+    dy:3
 }
-function modelLoaded() {
-    console.log("ml5 has sucessfully loaded");
-    Status = true;
+
+function setup(){
+  var canvas =  createCanvas(700,600);
+  video = createCapture(VIDEO);
+  video.size(800, 400);
+  video.parent('game_console');
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
-function gotResult(error, results) {
- if (error) {
-  console.log(error);
- } else {
-     console.log(results);
-     objects = results;
- }
+
+function draw(){
+
+ background(0); 
+
+ fill("black");
+ stroke("black");
+ rect(680,0,20,700);
+
+ fill("black");
+ stroke("black");
+ rect(0,0,20,700);
+ 
+   //funtion paddleInCanvas call 
+   paddleInCanvas();
+ 
+   //left paddle
+   fill(250,0,0);
+    stroke(0,0,250);
+    strokeWeight(0.5);
+   paddle1Y = mouseY; 
+   rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
+   
+   
+    //pc computer paddle
+    fill("#FFA500");
+    stroke("#FFA500");
+   var paddle2y =ball.y-paddle2Height/2;  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
+    
+    //function midline call
+    midline();
+    
+    //funtion drawScore call 
+   drawScore();
+   
+   //function models call  
+   models();
+   
+   //function move call which in very important
+    move();
+    
 }
-function preload() {
-    img = loadImage('https://images.pexels.com/photos/1973270/pexels-photo-1973270.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
+
+
+
+//function reset when ball does notcame in the contact of padde
+function reset(){
+   ball.x = width/2+100,
+   ball.y = height/2+100;
+   ball.dx=3;
+   ball.dy =3;
+   
 }
-function draw() {
-    image(img, 0, 0, 380, 380);
-    if(Status != "") {
-        r = random(255);
-        g = random(255);
-        b = random(255);
-        objectDetector.detect(img, gotResult);
-     for (i = 0; i < objects.length; i++) {
-         document.getElementById("status").innerHTML = "Status: Baby has been found";
-         document.getElementById("number_of_objects").innerHTML = "The number of babies are:"+ objects.length;
-         fill(r, g, b);
-         percent = floor(objects[i].confidence * 100);
-         text(objects[i].label + "" + percent + "%", objects[i].x, objects[i].y);
-         noFill();
-         stroke(r, g, b);
-         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height,);
-     }
+
+
+//function midline draw a line in center
+function midline(){
+    for(i=0;i<480;i+=10) {
+    var y = 0;
+    fill("white");
+    stroke(0);
+    rect(width/2,y+i,10,480);
     }
+}
+
+
+//function drawScore show scores
+function drawScore(){
+    textAlign(CENTER);
+    textSize(20);
+    fill("white");
+    stroke(250,0,0)
+    text("Player:",100,50)
+    text(playerscore,140,50);
+    text("Computer:",500,50)
+    text(pcscore,555,50)
+}
+
+
+//very important function of this game
+function move(){
+   fill(50,350,0);
+   stroke(255,0,0);
+   strokeWeight(0.5);
+   ellipse(ball.x,ball.y,ball.r,20)
+   ball.x = ball.x + ball.dx;
+   ball.y = ball.y + ball.dy;
+   if(ball.x+ball.r>width-ball.r/2){
+       ball.dx=-ball.dx-0.5;       
+   }
+  if (ball.x-2.5*ball.r/2< 0){
+  if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
+    ball.dx = -ball.dx+0.5; 
+  }
+  else{
+    pcscore++;
+    reset();
+    navigator.vibrate(100);
+  }
+}
+if(pcscore ==4){
+    fill("#FFA500");
+    stroke(0)
+    rect(0,0,width,height-1);
+    fill("white");
+    stroke("white");
+    textSize(25)
+    text("Game Over!☹☹",width/2,height/2);
+    text("Reload The Page!",width/2,height/2+30)
+    noLoop();
+    pcscore = 0;
+}
+   if(ball.y+ball.r > height || ball.y-ball.r <0){
+       ball.dy =- ball.dy;
+   }   
+}
+
+
+//width height of canvas speed of ball 
+function models(){
+    textSize(18);
+    fill(255);
+    noStroke();
+    text("Width:"+width,135,15);
+    text("Speed:"+abs(ball.dx),50,15);
+    text("Height:"+height,235,15)
+}
+
+
+//this function help to not go te paddle out of canvas
+function paddleInCanvas(){
+  if(mouseY+paddle1Height > height){
+    mouseY=height-paddle1Height;
+  }
+  if(mouseY < 0){
+    mouseY =0;
+  }  
 }
